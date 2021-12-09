2072b167fd271b3357361b077f890860d89d4dd1
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-09 16:50:16 +0530

    added humburger functionality

---
+++ tools.js
@@ -0,0 +1,37 @@
+let toolsCont = document.querySelector(".tools-cont");
+let optionsCont = document.querySelector(".options-cont");
+
+let pencilToolCont = document.querySelector(".pencil-tool-cont");
+let eraserToolCont = document.querySelector(".eraser-tool-cont");
+
+
+// true-show and false-hide
+let optionsFlag = true;
+
+optionsCont.addEventListener('click', (e) => {
+    optionsFlag = !optionsFlag;
+
+    if (optionsFlag) {
+        openTools();
+    } else {
+        closeTools();
+    }
+})
+
+function openTools() {
+
+    let iconElem = optionsCont.children[0];
+    iconElem.classList.remove("fa-times");
+    iconElem.classList.add("fa-bars");
+    toolsCont.style.display = "flex";
+}
+function closeTools() {
+
+    let iconElem = optionsCont.children[0];
+    iconElem.classList.remove("fa-bars");
+    iconElem.classList.add("fa-times");
+    toolsCont.style.display = "none";
+
+    pencilToolCont.style.display = "none";
+    eraserToolCont.style.display = "none";
+}
