8dbba36387b5d0f779ab3b8cebee57b5c99ab2bf
Sambhunath-Sahoo <sambhunathsahoo25@gmail.com>
2021-12-13 15:10:23 +0530

    pencil and eraser toll hide open is done

--- tools.js
+++ tools.js
@@ -1,14 +1,16 @@
 let toolsCont = document.querySelector(".tools-cont");
 let optionsCont = document.querySelector(".options-cont");
-
+let optionsFlag = true;
 let pencilToolCont = document.querySelector(".pencil-tool-cont");
 let eraserToolCont = document.querySelector(".eraser-tool-cont");
+let pencil = document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
+let sticky = document.querySelector(".sticky");
+let upload = document.querySelector(".upload");
+let pencilFlag = false;
+let eraserFlag = false;
 
-
-// true-show and false-hide
-let optionsFlag = true;
-
-optionsCont.addEventListener('click', (e) => {
+optionsCont.addEventListener("click", (e) => {
     optionsFlag = !optionsFlag;
 
     if (optionsFlag) {
@@ -16,17 +18,17 @@ optionsCont.addEventListener('click', (e) => {
     } else {
         closeTools();
     }
-})
+});
 
+// open hamburger
 function openTools() {
-
     let iconElem = optionsCont.children[0];
     iconElem.classList.remove("fa-times");
     iconElem.classList.add("fa-bars");
     toolsCont.style.display = "flex";
 }
+// close hamburger
 function closeTools() {
-
     let iconElem = optionsCont.children[0];
     iconElem.classList.remove("fa-bars");
     iconElem.classList.add("fa-times");
@@ -35,3 +37,19 @@ function closeTools() {
     pencilToolCont.style.display = "none";
     eraserToolCont.style.display = "none";
 }
+
+pencil.addEventListener("click", (e) => {
+    // true -> show pencil tool, false -> hide pencil tool
+    pencilFlag = !pencilFlag;
+
+    if (pencilFlag) pencilToolCont.style.display = "block";
+    else pencilToolCont.style.display = "none";
+});
+
+eraser.addEventListener("click", (e) => {
+    // true -> show eraser tool, false -> hide eraser tool
+    eraserFlag = !eraserFlag;
+
+    if (eraserFlag) eraserToolCont.style.display = "flex";
+    else eraserToolCont.style.display = "none";
+});
